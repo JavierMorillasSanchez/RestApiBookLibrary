9e973f8ca5d429ab83a386a0d82f288acc43326b
Javier MORILLAS SANCHEZ <javier.morillas@alten.es>
2025-03-19 11:16:16 +0100

    First commit

---
+++ src/test/java/com/restapibooklibrary/RestApiBookLibrary/RestApiBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapibooklibrary.RestApiBookLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
