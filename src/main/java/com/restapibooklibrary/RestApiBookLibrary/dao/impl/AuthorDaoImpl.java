5c4832ce31349e5992179c977aa36793e5604036
JavierMS <morillas.sanchez.91@gmail.com>
2025-03-20 15:49:27 +0100

    Implemented Test

--- src/main/java/com/restapibooklibrary/RestApiBookLibrary/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/restapibooklibrary/RestApiBookLibrary/dao/impl/AuthorDaoImpl.java
@@ -1,4 +1,13 @@
 package com.restapibooklibrary.RestApiBookLibrary.dao.impl;
 
-public class AuthorDaoImpl {
+import com.restapibooklibrary.RestApiBookLibrary.dao.AuthorDao;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class AuthorDaoImpl implements AuthorDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuthorDaoImpl(final JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
--- src/main/java/com/restapibooklibrary/RestApiBookLibrary/dao/impl/BookDaoImpl.java
+++ src/main/java/com/restapibooklibrary/RestApiBookLibrary/dao/impl/BookDaoImpl.java
@@ -1,4 +1,13 @@
 package com.restapibooklibrary.RestApiBookLibrary.dao.impl;
 
-public class BookDaoImpl {
+import com.restapibooklibrary.RestApiBookLibrary.dao.BookDao;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class BookDaoImpl implements BookDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDaoImpl(final JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
